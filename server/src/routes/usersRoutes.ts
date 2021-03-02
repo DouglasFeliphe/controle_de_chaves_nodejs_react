44d89d2e2a1356b9b73efc9f187d00f68c2cbc1a
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-02 12:16:26 -0300

    criação do userRoutes

---
+++ server/src/routes/usersRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const routes = express.Router()
+
+routes.get('/users')
+routes.get('/users/:id')
+routes.post('/user')
+routes.put('/users/:id')
+routes.delete('/users/:id')
+
+export default routes
