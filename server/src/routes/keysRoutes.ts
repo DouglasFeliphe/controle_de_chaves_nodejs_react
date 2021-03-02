7a99bb45b3d355a273fbdf703a9822a8782e7429
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-02 12:12:05 -0300

    separação das rotas para a pasta routes

--- server/src/routes/keysRoutes.ts
+++ server/src/routes/keysRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import KeysController from './controllers/KeysController';
+import KeysController from '../controllers/KeysController';
 
 const keysController = new KeysController()
 
