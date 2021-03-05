e20b8c250dd8154379f91e76f6898d9ec2b5e199
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-05 11:14:29 -0300

    criação das rotas mostrar, criar, atualizar e deletar

--- server/src/routes/reservationsRoutes.ts
+++ server/src/routes/reservationsRoutes.ts
@@ -5,5 +5,9 @@ const routes = express.Router()
 const reservationsController = new ReservationsController()
 
 routes.get('/reservations', reservationsController.index)
+routes.get('/reservations/:registration_number', reservationsController.show)
+routes.post('/reservation', reservationsController.create)
+routes.put('/reservations/:registration_number', reservationsController.update)
+routes.delete('/reservations/:registration_number', reservationsController.delete)
 
 export default routes
