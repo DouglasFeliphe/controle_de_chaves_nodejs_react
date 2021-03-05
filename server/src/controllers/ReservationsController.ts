acb5bce8a87b80d177b6d648f7cbe1d47be553f6
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-05 10:41:05 -0300

    criação do controller de reservas

---
+++ server/src/controllers/ReservationsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import connection from '../database/connection';
+
+class ReservationsController {
+
+    async index(request: Request, Response: Response) {
+
+    }
+
+    async create(request: Request, response: Response) {
+
+    }
+
+    async show(request: Request, response: Response) {
+
+    }
+
+    async update(request: Request, response: Response) {
+
+    }
+
+    async delete(request: Request, response: Response) {
+
+    }
+}
