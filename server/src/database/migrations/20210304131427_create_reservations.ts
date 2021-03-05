d8ee0e17b9e0d1f1e81fb31809f3f7e3aba0872d
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-05 10:14:41 -0300

    correção na migration e na seed de reservas

--- server/src/database/migrations/20210304131427_create_reservations.ts
+++ server/src/database/migrations/20210304131427_create_reservations.ts
@@ -5,8 +5,9 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('reservations', table => {
         table.increments('id').primary()
         table.string('key_name').notNullable()
-        table.integer('key_number').notNullable()
+        table.integer('key_number').references('key_number').inTable('keys')
         table.string('user_name').notNullable()
+        table.integer('user_registration_number').references('registration_number').inTable('users')
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.timestamp('returned_at').defaultTo(knex.fn.now())
     })
--- server/src/database/seeds/insert_reservations.ts
+++ server/src/database/seeds/insert_reservations.ts
@@ -6,8 +6,8 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("table_name").insert([
-        { key_name: "lab01", key_number: 1, user_name: 'foo' },
-        { key_name: "lab02", key_number: 2, user_name: 'bar' },
-        { key_name: "lab03", key_number: 2, user_name: 'baz' }
+        { key_name: "lab01", key_number: 1, user_name: 'foo', user_reg_number: 1000000001 },
+        { key_name: "lab02", key_number: 2, user_name: 'bar', user_reg_number: 2000000001 },
+        { key_name: "lab03", key_number: 3, user_name: 'baz', user_reg_number: 3000000001 }
     ]);
 };
