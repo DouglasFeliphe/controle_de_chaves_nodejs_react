3f5d98250bc8193ac55ef46efeae000403cad728
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-03-05 10:35:51 -0300

    correção na seed de reservas

--- server/src/database/seeds/insert_reservations.ts
+++ server/src/database/seeds/insert_reservations.ts
@@ -2,12 +2,12 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("table_name").del();
+    await knex("reservations").del();
 
     // Inserts seed entries
-    await knex("table_name").insert([
-        { key_name: "lab01", key_number: 1, user_name: 'foo', user_reg_number: 1000000001 },
-        { key_name: "lab02", key_number: 2, user_name: 'bar', user_reg_number: 2000000001 },
-        { key_name: "lab03", key_number: 3, user_name: 'baz', user_reg_number: 3000000001 }
+    await knex("reservations").insert([
+        { key_name: "lab01", key_number: 1, user_name: 'foo', user_registration_number: 1000000001 },
+        { key_name: "lab02", key_number: 2, user_name: 'bar', user_registration_number: 2000000001 },
+        { key_name: "lab03", key_number: 3, user_name: 'baz', user_registration_number: 3000000001 }
     ]);
 };
